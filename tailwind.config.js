92112e2e3d46b6ef0b77cb74653133c8698fa7fe
TheVoidThatConsumes <lyricalluxor@gmail.com>
2023-03-20 08:58:45 +0100

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'Sans-serif']
+      },
+      colors: {
+        'body': ['#302C42']
+      }
+    },
+  },
+  plugins: [],
+}
