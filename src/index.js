7bcf1f39e7a21283fca3670d9c5574bcbf9c42c3
TheVoidThatConsumes <lyricalluxor@gmail.com>
2023-04-02 12:17:16 +0200

    Links and Hamburger
    Linked Sections and Built the Hamburger menu from scratch.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,12 @@
+const burger = document.getElementById('burger');
 
+function showMenu(){
+    burger.classList.remove('opacity-0', 'h-0', 'w-0');
+    burger.classList.add('opacity-85', 'w-full', 'h-full');
+}
+
+function hideMenu(){
+    burger.classList.remove('opacity-85', 'w-full', 'h-full');
+    burger.classList.add('opacity-0', 'h-0', 'w-0');
+}
 
